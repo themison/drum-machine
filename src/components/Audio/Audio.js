fa53f86a704a212f15f062ea5a47c9423f2584c9
Ilya Girsenok <ilya.girsenok@gmail.com>
2019-04-18 21:25:02 +0400

    changed a bit for passing tests

--- src/components/Audio/Audio.js
+++ src/components/Audio/Audio.js
@@ -3,9 +3,7 @@ import React, { Component } from "react";
 export default class Audio extends Component {
   render() {
     return (
-    <div>
       <audio data-key={this.props.dataKey} src={this.props.audio} id={this.props.id} className='clip'></audio>
-    </div>
     )
   }
 }
